00b745d351a7e4dc8b85cb3dceb7f4b1b84e7892
Matt Munch <mattmunch94@gmail.com>
2019-12-10 13:56:18 -0800

    delete route test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -119,4 +119,32 @@ describe('app routes', () => {
         });
       });
   });
+  it('deletes a recipe', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+
+    return request(app)
+      .delete(`/api/v1/recipes/${recipe.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          _id: recipe._id.toString(),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ]
+        });
+      
+      });
+  });
 });
